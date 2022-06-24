5ee479a97c8e64b16a3d971fa91c4deb49e5e4c2
sandeep2315 <sandeepgowda2314@gmail.com>
2022-06-24 15:56:37 +0530

    24-06-2022

--- electrical/app1/models.py
+++ electrical/app1/models.py
@@ -64,16 +64,16 @@ class userphoto(models.Model):
 class my_account(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     # photo=models.ImageField(upload_to='profile', verbose_name="Profle photo", null=True, blank=True, max_length=500)
-    first_name=models.CharField(max_length=50,null=True,verbose_name="First Name",default="User")
-    last_name=models.CharField(max_length=50,null=True,verbose_name="last Name")
-    phone_number=models.BigIntegerField(null=True,verbose_name="Phone number")
-    email=models.EmailField(max_length=255,null=True,verbose_name="Email")
-    address=models.TextField(null=True,verbose_name="Address")
-    city=models.CharField(max_length=100,null=True,verbose_name="City")
-    state=models.CharField(max_length=50,null=True,verbose_name="State")
+    first_name=models.CharField(max_length=50,null=True,blank=True,verbose_name="First Name",default="User")
+    last_name=models.CharField(max_length=50,null=True,blank=True,verbose_name="last Name")
+    phone_number=models.BigIntegerField(null=True,blank=True,verbose_name="Phone number")
+    email=models.EmailField(max_length=255,null=True,blank=True,verbose_name="Email")
+    address=models.TextField(null=True,blank=True,verbose_name="Address")
+    city=models.CharField(max_length=100,null=True,blank=True,verbose_name="City")
+    state=models.CharField(max_length=50,null=True,blank=True,verbose_name="State")
     is_confirmed = models.BooleanField(default=False)
     otp = models.IntegerField(default=False,null=True,blank=True)
-    postal_pin=models.BigIntegerField(null=True,verbose_name="Postal address")
+    postal_pin=models.BigIntegerField(null=True,blank=True,verbose_name="Postal address")
     
     # def __str__(self):
     #     return self.first_name    
